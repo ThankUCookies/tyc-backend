62d061e5188534df0d3dc4673046cfa69644ab90
Ameer Jhan <ameerjhanprof@gmail.com>
2019-06-30 17:33:56 +0530

    fix: take the datetime from the client

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -101,19 +101,16 @@ transactionRoute.post(
   "/add-skus",
   [body("skuCodes").exists(), 
   body("typeId").exists(), 
-  body("eventId").exists()],
+  body("eventId").exists(),
+  body("dateTime").exists()],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
-     // set date to IST
-    const today = new Date();
-    today.setHours(today.getUTCHours() + 5);
-    today.setMinutes(today.getUTCMinutes() + 30);
 
     if (errors.isEmpty()) {
       try {
         // TODO: use UTC timestamp
         const transaction = await transactionBusinessAccess.create({
-          dateTime: today,
+          dateTime: req.body.dateTime,
           eventId: req.body.eventId,
           typeId: req.body.typeId,
           userId: req.user.id,
